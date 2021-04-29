c42c15e7bfa6d7be04cc3ac08c19b7090beedeea
Chenboning <Boning.Chen@mobilityasia.com.cn>
2021-04-29 12:21:23 +0800

    @dev esmini cmd path

--- openscenario/simulator/esmini.py
+++ openscenario/simulator/esmini.py
@@ -1,5 +1,5 @@
 import os
-from .constants import ESMINI_PATH
+from .constants import ESMINI_CMD_PATH
 from .constants import ESMINI_SAMPLE_PATH
 import os
 
@@ -14,11 +14,12 @@ def record_gif(xodr_file_name, xosc_file_name, total_time):
 
     __exec_cmd('Xvfb :1 -screen 0 ${RESOLUTION_X}x${RESOLUTION_Y}x24 &> xvfb.log &')
     gif_name = xosc_file_name.replace('.xosc', '.gif')
-    esmini_cmd = 'timeout $LENGTH esmini --osc %s%s --window 0 0 ${RESOLUTION_X} ${RESOLUTION_Y} --camera_mode top &' % (
-        ESMINI_SAMPLE_PATH, xosc_file_name)
+    esmini_cmd = 'timeout $LENGTH %s --osc %s%s --window 0 0 ${RESOLUTION_X} ${RESOLUTION_Y} --camera_mode top &' % (
+        ESMINI_CMD_PATH, ESMINI_SAMPLE_PATH, xosc_file_name)
     __exec_cmd(esmini_cmd)
 
-    __exec_cmd('avconv -f x11grab -s ${RESOLUTION_X}x${RESOLUTION_Y} -r $FRAMERATE -t $LENGTH -y -i :1.0 \"%s\"' % gif_name)
+    __exec_cmd(
+        'avconv -f x11grab -s ${RESOLUTION_X}x${RESOLUTION_Y} -r $FRAMERATE -t $LENGTH -y -i :1.0 \"%s\"' % gif_name)
 
     return '%s%s' % (ESMINI_SAMPLE_PATH, gif_name)
 
