a4d453a002220c93329b41da955f768baaf0109f
Chenboning <Boning.Chen@mobilityasia.com.cn>
2021-04-29 16:29:03 +0800

    @dev adjust gif args

--- openscenario/simulator/esmini.py
+++ openscenario/simulator/esmini.py
@@ -1,25 +1,27 @@
 import os
+
 from .constants import ESMINI_CMD_PATH
 from .constants import ESMINI_SAMPLE_PATH
-import os
 
 
-def record_gif(xodr_file_name, xosc_file_name, total_time):
+def record_gif(xodr_file_name, xosc_file_name, length=10, resolution_x=320, resolution_y=240, frame_rate=10):
     if not os.path.exists(ESMINI_SAMPLE_PATH):
         os.mkdir(ESMINI_SAMPLE_PATH)
 
     __exec_cmd('mv %s %s' % (xodr_file_name, ESMINI_SAMPLE_PATH))
     __exec_cmd('mv %s %s' % (xosc_file_name, ESMINI_SAMPLE_PATH))
-    __exec_cmd('export LENGTH=%d' % total_time)
+    print('gif configs, length=%s, resolution=%sx%s, frame_rate=%s' % (
+        str(length), str(resolution_x), str(resolution_y), str(frame_rate)))
 
-    __exec_cmd('Xvfb :1 -screen 0 ${RESOLUTION_X}x${RESOLUTION_Y}x24 -nocursor &> xvfb.log &')
+    __exec_cmd('Xvfb :1 -screen 0 %dx%dx24 -nocursor &> xvfb.log &' % (resolution_x, resolution_y))
     gif_name = xosc_file_name.replace('.xosc', '.gif')
-    esmini_cmd = 'timeout $LENGTH %s --osc %s%s --window 0 0 ${RESOLUTION_X} ${RESOLUTION_Y} --camera_mode top &' % (
-        ESMINI_CMD_PATH, ESMINI_SAMPLE_PATH, xosc_file_name)
+    esmini_cmd = 'timeout %d %s --osc %s%s --window 0 0 %d %d --camera_mode top &' % (
+        length, ESMINI_CMD_PATH, ESMINI_SAMPLE_PATH, xosc_file_name, resolution_x, resolution_y)
     __exec_cmd(esmini_cmd)
     gif_save_path = ESMINI_SAMPLE_PATH + '/' + gif_name
     __exec_cmd(
-        'avconv -f x11grab -s ${RESOLUTION_X}x${RESOLUTION_Y} -draw_mouse 0 -r $FRAMERATE -t $LENGTH -y -i :1.0 \"%s\"' % gif_save_path)
+        'avconv -f x11grab -s %dx%d -draw_mouse 0 -r %d -t %d -y -i :1.0 \"%s\"' % (
+            resolution_x, resolution_y, frame_rate, length, gif_save_path))
 
     return gif_save_path
 
