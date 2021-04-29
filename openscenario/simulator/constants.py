2b0dffb2fddb818d8954b8ad1159a4517236f00a
Chenboning <Boning.Chen@mobilityasia.com.cn>
2021-04-29 12:28:16 +0800

    @dev adjust esmni

--- openscenario/simulator/constants.py
+++ openscenario/simulator/constants.py
@@ -1,4 +1,4 @@
 ESMINI_PATH = '/root/esmini-self'
 VEHICLE_CATALOG = ESMINI_PATH + '/resources/xosc/Catalogs/Vehicles'
 ESMINI_SAMPLE_PATH = ESMINI_PATH + '/resources/samples/'
-ESMINI_CMD_PATH = ESMINI_PATH + '/build/sEnvironmentSimulator/Application/esmini/esmini'
+ESMINI_CMD_PATH = ESMINI_PATH + '/build/EnvironmentSimulator/Applications/esmini/esmini'
